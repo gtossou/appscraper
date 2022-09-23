bf43103383598da7c2aefae0c5527c3dd2c6de98
Koffi <gil.tossou@gmail.com>
2022-09-23 14:50:20 +0200

    refactor models

--- models/scrappermodel.py
+++ models/scrappermodel.py
@@ -3,9 +3,9 @@ SQL MODEL for appstats
 """
 
 from datetime import datetime
-from typing import Optional
+from typing import List, Optional
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
 
 class AppInfo(SQLModel, table=True):
@@ -13,9 +13,11 @@ class AppInfo(SQLModel, table=True):
     title: str = Field(default=None)
     description: str = Field(default=None)
     summary: str = Field(default=None)
+    stats: List["AppStats"] = Relationship(back_populates="appinfo")
 
 
 class AppStats(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     installs: str = Field(index=True)
     min_installs: int = Field(default=0)
     real_installs: int = Field(default=0)
@@ -23,6 +25,6 @@ class AppStats(SQLModel, table=True):
     ratings: int = Field(default=0)
     reviews: int = Field(default=0)
     insert_time: datetime = Field(
-        default_factory=datetime.utcnow, nullable=False, primary_key=True)
-    app_id: str = Field(
-        default=None, primary_key=True)
+        default_factory=datetime.utcnow, nullable=False)
+    app_id: Optional[int] = Field(default=None, foreign_key="appinfo.id")
+    info: Optional[AppInfo] = Relationship(back_populates="appstats")
