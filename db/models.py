9bd7bfa54575364bd2103c443ed3831f99d6be1d
Koffi <gil.tossou@gmail.com>
2022-10-07 09:57:28 +0200

    fix models

--- db/models.py
+++ db/models.py
@@ -10,31 +10,32 @@ from sqlmodel import Field, Relationship, SQLModel
 
 class AppCollect(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    username: str = Field(default=None)
-    email: str = Field(default=None)
-    appname: str = Field(default=None)
-    appurl: str = Field(default=None)
+    username: str = None
+    email: str = None
+    appname: str = None
+    appurl: str = None
 
 
 class AppInfo(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    appurl: str = Field(default=None)
-    title: str = Field(default=None)
-    description: str = Field(default=None)
-    summary: str = Field(default=None)
+    appurl: str = None
+    title: str = None
+    description: str = None
+    summary: str = None
     last_update: datetime = Field(
         default_factory=datetime.utcnow, nullable=False)
+    approved: Optional[bool] = True
     stats: List["AppStats"] = Relationship(back_populates="appinfo")
 
 
 class AppStats(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     installs: str = Field(index=True)
-    min_installs: int = Field(default=0)
-    real_installs: int = Field(default=0)
-    score: float = Field(default=0)
-    ratings: int = Field(default=0)
-    reviews: int = Field(default=0)
+    min_installs: int = 0
+    real_installs: int = 0
+    score: float = 0
+    ratings: int = 0
+    reviews: int = 0
     last_update: datetime = Field(
         default_factory=datetime.utcnow, nullable=False)
     app_id: Optional[int] = Field(default=None, foreign_key="appinfo.id")
